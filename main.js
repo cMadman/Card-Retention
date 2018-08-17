35ec63d9cbe6003d8c596ed70a0daaad446c590d
Mark Cadman <github@cadman.co>
2018-08-17 21:48:09 +0100

    About to abandon lodash as this is more pain than is worth it. I blame you Rolands!

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var _ = require("lodash");
 // initialisation
 window.onload = load;
 // system functions
@@ -5,25 +8,59 @@ function load() {
     initialise();
 }
 function initialise() {
-    // and then we begin
+    var players = [
+        new Player("Mark")
+    ];
+    var game = new Game(players, "Mark", "Easy", 500, 500);
+    game.createBoard;
 }
-var Table = /** @class */ (function () {
-    function Table(width, height) {
+var Game = /** @class */ (function () {
+    function Game(players, currentPlayer, difficulty, width, height) {
+        this.players = players;
+        this.currentPlayer = currentPlayer;
+        this.difficulty = difficulty;
         this.width = width;
         this.height = height;
-        // what goes here?
+        if (difficulty == "Easy") {
+            this.rows = 4;
+            this.columns = 4;
+            this.totalCards = this.rows * this.columns;
+        }
     }
-    return Table;
-}());
-var Game = /** @class */ (function () {
-    function Game(type) {
-        this.type = type;
-        // what goes here?
-    }
+    Game.prototype.createBoard = function () {
+        this.board = document.createElement('div');
+        this.board.style = "\n            background-color: #eee;\n            height: " + this.height + ";\n            left: 0;\n            position: absolute;\n            top: 0;\n            width: " + this.width + ";\n        ";
+        document.body.appendChild(this.board);
+    };
+    Game.prototype.deal = function () {
+    };
+    Game.prototype.start = function () {
+        var cards = [
+            "🍌", "🍉", "🍇", "🍓", "🍒", "🍑", "🍍", "🥥", "🥝", "🍆", "🥑", "🥦", "🌽", "🥕", "🍠"
+        ];
+        this.deck = new Deck(cards, this.totalCards);
+        this.deal;
+    };
     return Game;
 }());
-var Layout = /** @class */ (function () {
-    function Layout() {
+var Player = /** @class */ (function () {
+    function Player(nickname) {
+        this.nickname = nickname;
     }
-    return Layout;
+    return Player;
+}());
+var Deck = /** @class */ (function () {
+    function Deck(cards, size) {
+        this.cards = cards;
+        this.size = size;
+        // cut
+        this.cards.slice(0, size / 2);
+        // fill
+        this.cards = this.cards.reduce(function (res, current, index, array) {
+            return res.concat([current, current]);
+        }, []);
+        // shuffle
+        this.cards = _.shuffle(this.cards);
+    }
+    return Deck;
 }());
--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+import * as _ from "lodash";
+
 // initialisation
 window.onload = load;
 
@@ -7,21 +9,80 @@ function load() {
 }
 
 function initialise() {
-  // and then we begin
-}
+  let players = [
+    new Player("Mark")
+  ];
 
-class Table {
-    constructor(public width: number, public height: number) {
-        // what goes here?
-    }
+  let game = new Game(players,"Mark","Easy",500,500);
+  game.createBoard;
 }
 
 class Game {
-    constructor(public type: string) {
-        // what goes here?
+    deck: Deck;
+    rows: number;
+    columns: number;
+    totalCards: number;
+    board: any;
+
+    constructor(
+        public players: Array<any>, 
+        public currentPlayer: string, 
+        public difficulty: string,
+        public width: number,
+        public height: number
+    ) {
+        if(difficulty == "Easy") {
+            this.rows = 4;
+            this.columns = 4;
+            this.totalCards = this.rows * this.columns;
+        }
+    }
+
+    createBoard() {
+        this.board = document.createElement('div');
+        this.board.style = `
+            background-color: #eee;
+            height: ${this.height};
+            left: 0;
+            position: absolute;
+            top: 0;
+            width: ${this.width};
+        `;
+        document.body.appendChild(this.board);
+    }
+
+    deal() {
+    }
+
+    start() {
+        let cards = [
+            "🍌","🍉","🍇","🍓","🍒","🍑","🍍","🥥","🥝","🍆","🥑","🥦","🌽","🥕","🍠"
+        ]
+        this.deck = new Deck(cards,this.totalCards);
+        this.deal;
     }
 }
 
-class Layout {
-    
+class Player {
+    constructor(
+        public nickname: string
+    ) {}
+}
+
+class Deck {
+    constructor(
+        public cards: Array<string>,
+        public size: number
+    ) {
+        // cut
+        this.cards.slice(0,size/2);
+
+        // fill
+        this.cards = this.cards.reduce(function (res, current, index, array) {
+            return res.concat([current, current]);
+          }, []);
+
+        // shuffle
+        this.cards = _.shuffle(this.cards);
+    }
 }

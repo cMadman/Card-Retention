ef6f8736fe287731ff4a7d7168b6865b72eee1fd
Mark Cadman <github@cadman.co>
2018-08-17 23:53:21 +0100

    OOP equivalent to where I was?

--- main.js
+++ main.js
@@ -8,35 +8,63 @@ function initialise() {
     var players = [
         new Player("Mark")
     ];
-    var game = new Game(players, "Mark", "Easy", 500, 500);
+    var game = new Game(players, "Mark", "Easy");
     game.createBoard();
+    game.deal();
 }
 var Game = /** @class */ (function () {
-    function Game(players, currentPlayer, difficulty, width, height) {
+    function Game(players, currentPlayer, difficulty) {
         this.players = players;
         this.currentPlayer = currentPlayer;
         this.difficulty = difficulty;
-        this.width = width;
-        this.height = height;
+        this.width = 500;
+        this.height = 500;
+        this.left = 50;
+        this.top = 50;
+        this.margin = 10;
+        this.tableau = [];
         if (difficulty == "Easy") {
             this.rows = 4;
             this.columns = 4;
             this.totalCards = this.rows * this.columns;
         }
+        this.aCard = Card.calculateCard({
+            width: this.width,
+            height: this.height,
+            margin: this.margin,
+            columns: this.columns,
+            rows: this.rows
+        });
     }
     Game.prototype.createBoard = function () {
         this.board = document.createElement('div');
-        this.board.style = "\n            background-color: #eee;\n            height: " + this.height + ";\n            left: 0;\n            position: absolute;\n            top: 0;\n            width: " + this.width + ";\n        ";
+        this.board.id = "game-board";
+        this.board.style = "\n            background-color: #eee;\n            height: " + this.height + ";\n            left: " + this.left + ";\n            position: relative;\n            top: " + this.top + ";\n            width: " + this.width + ";\n        ";
         document.body.appendChild(this.board);
     };
     Game.prototype.deal = function () {
-    };
-    Game.prototype.start = function () {
         var cards = [
             "🍌", "🍉", "🍇", "🍓", "🍒", "🍑", "🍍", "🥥", "🥝", "🍆", "🥑", "🥦", "🌽", "🥕", "🍠"
         ];
         this.deck = new Deck(cards, this.totalCards);
-        this.deal;
+        var pointer = {
+            left: this.margin,
+            top: this.margin
+        };
+        for (var row = 0; row < this.rows; row++) {
+            this.tableau[row] = [];
+            for (var col = 0; col < this.columns; col++) {
+                this.tableau[row][col] = new Card(pointer.top, pointer.left, this.aCard.width, this.aCard.height, this.deck.cards.pop());
+                pointer.left = pointer.left + this.aCard.width + this.margin;
+            }
+            pointer.left = this.margin;
+            pointer.top = pointer.top + this.aCard.height + this.margin;
+        }
+        this.tableau.forEach(function (row) {
+            row.forEach(function (card) {
+                document.getElementById("game-board").appendChild(card.html); // for some reason this.board.appendChild errored, seemed like a scoping issue TODO return this to a reference
+            });
+        });
     };
     return Game;
 }());
@@ -45,6 +73,40 @@ var Player = /** @class */ (function () {
         this.nickname = nickname;
     }
     return Player;
+}());
+var Card = /** @class */ (function () {
+    function Card(top, left, width, height, content) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+        this.content = content;
+        this.html = document.createElement('div');
+        this.html.className = 'flip-container';
+        this.html.style.height = height;
+        this.html.style.left = left;
+        this.html.style.position = 'absolute';
+        this.html.style.top = top;
+        this.html.style.width = width;
+        this.html.addEventListener('click', this.flip);
+        this.html.innerHTML = "\n          <div class=\"flipper\">\n            <div class=\"front\"></div>\n            <div class=\"back\">" + content + "</div>\n          </div>\n        ";
+    }
+    Card.prototype.flip = function () {
+        this.classList.toggle("flip");
+    };
+    Card.calculateCard = function (options) {
+        var cardWidth = (options.width -
+            ((options.columns * options.margin) + options.margin))
+            / options.columns;
+        var cardHeight = (options.height -
+            ((options.rows * options.margin) + options.margin))
+            / options.rows;
+        return {
+            width: cardWidth,
+            height: cardHeight
+        };
+    };
+    return Card;
 }());
 var Deck = /** @class */ (function () {
     function Deck(cards, size) {
--- main.ts
+++ main.ts
@@ -11,8 +11,9 @@ function initialise() {
     new Player("Mark")
   ];
 
-  let game = new Game(players,"Mark","Easy",500,500);
+  let game = new Game(players,"Mark","Easy");
   game.createBoard();
+  game.deal();
 }
 
 class Game {
@@ -21,43 +22,77 @@ class Game {
     columns: number;
     totalCards: number;
     board: any;
+    aCard: any;
+
+    width = 500;
+    height = 500;
+    left = 50;
+    top = 50;
+    margin = 10;
+    tableau = [];
 
     constructor(
         public players: Array<any>, 
         public currentPlayer: string, 
-        public difficulty: string,
-        public width: number,
-        public height: number
+        public difficulty: string
     ) {
         if(difficulty == "Easy") {
             this.rows = 4;
             this.columns = 4;
             this.totalCards = this.rows * this.columns;
         }
+        this.aCard = Card.calculateCard({
+            width: this.width,
+            height: this.height,
+            margin: this.margin,
+            columns: this.columns,
+            rows: this.rows
+        });
     }
 
     createBoard() {
         this.board = document.createElement('div');
+        this.board.id = "game-board";
         this.board.style = `
             background-color: #eee;
             height: ${this.height};
-            left: 0;
-            position: absolute;
-            top: 0;
+            left: ${this.left};
+            position: relative;
+            top: ${this.top};
             width: ${this.width};
         `;
         document.body.appendChild(this.board);
     }
 
     deal() {
-    }
-
-    start() {
         let cards = [
             "🍌","🍉","🍇","🍓","🍒","🍑","🍍","🥥","🥝","🍆","🥑","🥦","🌽","🥕","🍠"
         ]
         this.deck = new Deck(cards,this.totalCards);
-        this.deal;
+        
+        let pointer = {
+            left: this.margin,
+            top: this.margin
+          }
+        
+          for (let row = 0; row < this.rows; row++) {
+            this.tableau[row] = [];
+        
+            for (let col = 0; col < this.columns; col++) {
+              this.tableau[row][col] = new Card(pointer.top,pointer.left,this.aCard.width,this.aCard.height,this.deck.cards.pop());
+        
+              pointer.left = pointer.left + this.aCard.width + this.margin;
+            }
+        
+            pointer.left = this.margin;
+            pointer.top = pointer.top + this.aCard.height + this.margin;
+          }
+
+        this.tableau.forEach(function(row) {
+            row.forEach(function(card) {
+                document.getElementById("game-board").appendChild(card.html); // for some reason this.board.appendChild errored, seemed like a scoping issue TODO return this to a reference
+            });
+        });
     }
 }
 
@@ -65,6 +100,63 @@ class Player {
     constructor(
         public nickname: string
     ) {}
+}
+
+class Card {
+    html: any;
+
+    constructor(
+        public top: number,
+        public left: number,
+        public width: number,
+        public height: number,
+        public content: string
+    ) {
+        this.html = document.createElement('div');
+        this.html.className = 'flip-container';
+        this.html.style.height = height;
+        this.html.style.left = left;
+        this.html.style.position = 'absolute';
+        this.html.style.top = top;
+        this.html.style.width = width;
+        this.html.addEventListener('click', this.flip);
+      
+        this.html.innerHTML = `
+          <div class="flipper">
+            <div class="front"></div>
+            <div class="back">${content}</div>
+          </div>
+        `;
+    }
+
+    flip() {
+        this.classList.toggle("flip");  // 'this' is annoying TODO ask Rols about this
+    }
+
+    static calculateCard(options) {
+        let cardWidth =
+          (
+            options.width -
+              (
+                (options.columns * options.margin) + options.margin
+              )
+          )
+          / options.columns;
+      
+          let cardHeight =
+            (
+              options.height -
+                (
+                  (options.rows * options.margin) + options.margin
+                )
+            )
+            / options.rows;
+      
+        return {
+            width: cardWidth,
+            height: cardHeight
+        }
+      }
 }
 
 class Deck {
